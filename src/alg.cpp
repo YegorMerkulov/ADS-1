4fc6d1abb8b44807826998def18d43022a846156
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-03-19 20:57:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,38 +14,32 @@ int val(int n){
 }
 
 uint64_t nPrime(uint64_t n) {
-int val(int *s, int n){
   for(int i=0; i<n;i++){
-    if(s[i]==1 || s[i]==2 || s[i]==3 || s[i]==5 || s[i]==7)
-      return s[i];
-    else if(s[i]%2!=0 && s[i]%3!=0 && s[i]%5!=0 && s[i]%7!=0)
-      return s[i];
+    if(i==1 || i==2 || i==3 || i==5 || i==7)
+      return i;
+    else if(i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0)
+      return i;
   }
-}
 }
 
 uint64_t nextPrime(uint64_t value) {
-int val(int n){
-  int count=0
+  int count=0;
   if(count!=0)
-    if(n==1 || n==2 || n==3 || n==5 || n==7)
-      return n;
-    else if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
-      return n;
+    if(value==1 || value==2 || value==3 || value==5 || value==7)
+      return value;
+    else if(value%2!=0 && value%3!=0 && value%5!=0 && value%7!=0)
+      return value;
   count+=1;
-}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-int val(int n){
   int sum=0;
-  if(n==1 || n==2 || n==3 || n==5 || n==7){
-    sum+=n;
-    return n+1;
-  }else if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0){
-    sum+=n;
-    return n+1;
+  if(hbound==1 || hbound==2 || hbound==3 || hbound==5 || hbound==7){
+    sum+=hbound;
+    return hbound+1;
+  }else if(hbound%2!=0 && hbound%3!=0 && hbound%5!=0 && hbound%7!=0){
+    sum+=hbound;
+    return hbound+1;
   }else
-    return n+1;
-}
+    return hbound+1;
 }
