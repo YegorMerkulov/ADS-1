1f38fde2a38ea1f820abda69311ab831dd8c18da
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-03-19 20:35:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,11 +15,12 @@ int val(int n){
 
 uint64_t nPrime(uint64_t n) {
 int val(int *s, int n){
-  for(int i=0; i<n;i++)
+  for(int i=0; i<n;i++){
     if(s[i]==1 || s[i]==2 || s[i]==3 || s[i]==5 || s[i]==7)
-    return s[i];
-  else if(s[i]%2!=0 && s[i]%3!=0 && s[i]%5!=0 && s[i]%7!=0)
-    return s[i];
+      return s[i];
+    else if(s[i]%2!=0 && s[i]%3!=0 && s[i]%5!=0 && s[i]%7!=0)
+      return s[i];
+  }
 }
 }
 
@@ -38,13 +39,13 @@ int val(int n){
 uint64_t sumPrime(uint64_t hbound) {
 int val(int n){
   int sum=0;
-  if(n==1 || n==2 || n==3 || n==5 || n==7)
+  if(n==1 || n==2 || n==3 || n==5 || n==7){
     sum+=n;
     return n+1;
-  else if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
+  }else if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0){
     sum+=n;
     return n+1;
-  else
+  }else
     return n+1;
 }
 }
