bd6d37e6a29182c67b5668162d2613f701774573
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-04-07 01:55:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,20 +13,21 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  int result;
-  for (uint64_t i=0; i < n; i++) {
-    if (i == 1 || i == 2 || i == 3 || i == 5 || i == 7) {
-      result=i;
-    } else if (i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0) {
-      result=i;
+  int result=0;
+  if(ressult==0){
+    if (n == 1 || n == 2 || n == 3 || n == 5 || n == 7) {
+      result=n;
+    } else if (n%2 != 0 && n%3 != 0 && n%5 != 0 && n%7 != 0) {
+      result=n;
+    } else {
+      n+=1;
     }
   }
-  return result;
 }
 
 uint64_t nextPrime(uint64_t value) {
   int result=0;
-  while (result=0)  {
+  while (result==0)  {
     value+=1;
     if (value == 1 || value == 2 || value == 3 || value == 5 || value == 7) {
       result=value;
@@ -39,13 +40,11 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   int sum = 0;
-  if (hbound == 1 || hbound == 2 || hbound == 3 || hbound == 5 || hbound == 7) {
-    sum+=hbound;
-    return hbound+1;
-  } else if (hbound%2 != 0 && hbound%3 != 0 && hbound%5 != 0 && hbound%7 != 0) {
-    sum+=hbound;
-    return hbound+1;
-  } else {
-    return hbound+1;
-  }
+  for (int i=0; i<hbound; i++)
+    if (i == 1 || i == 2 || i == 3 || i == 5 || i == 7) {
+      sum+=i;
+    } else if (i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0) {
+      sum+=i;
+    }
+  return sum;
 }
