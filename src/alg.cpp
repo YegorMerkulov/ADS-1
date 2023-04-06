c42252be6368a3896363946f61b7e23fa37b523d
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-04-07 02:12:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,4 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
@@ -13,12 +14,12 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  int result=0;
-  while(result==0){
+  int result = 0;
+  while (result == 0) {
     if (n == 1 || n == 2 || n == 3 || n == 5 || n == 7) {
-      result=n;
+      result = n;
     } else if (n%2 != 0 && n%3 != 0 && n%5 != 0 && n%7 != 0) {
-      result=n;
+      result = n;
     } else {
       n+=1;
     }
@@ -27,13 +28,13 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int result=0;
-  while (result==0)  {
+  int result = 0;
+  while (result == 0)  {
     value+=1;
     if (value == 1 || value == 2 || value == 3 || value == 5 || value == 7) {
-      result=value;
+      result = value;
     } else if (value%2 != 0 && value%3 != 0 && value%5 != 0 && value%7 != 0) {
-      result=value;
+      result = value;
     }
   }
   return result;
@@ -41,11 +42,12 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   int sum = 0;
-  for (int i=0; i<hbound; i++)
+  for (int i=0; i < hbound; i++) {
     if (i == 1 || i == 2 || i == 3 || i == 5 || i == 7) {
       sum+=i;
     } else if (i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0) {
       sum+=i;
     }
+  }
   return sum;
 }
