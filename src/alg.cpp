7e5763dd8a8cd610f05e1881ae323dcf52ee3fcb
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-04-07 17:18:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,10 +5,9 @@
 
 
 bool checkPrime(uint64_t value) {
-  for (int i = 2; i <= sqrt(value); ++i) {
-    if (value%i == 0) {
+for (int i = 2; i <= sqrt(value); ++i) {
+    if (value % i == 0)
       return false;
-    }
   }
   return true;
 }
@@ -17,33 +16,26 @@ uint64_t nPrime(uint64_t n) {
   int i = 0;
   int count = 0;
   while (count != n) {
+    i+=1;
     if (checkPrime(i)) {
       count+=1;
     }
-     i+=1;
   }
   return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int result = 0;
-  while (result == 0)  {
-    value+=1;
-    if (value == 1 || value == 2 || value == 3 || value == 5 || value == 7) {
-      result = value;
-    } else if (value%2 != 0 && value%3 != 0 && value%5 != 0 && value%7 != 0) {
-      result = value;
-    }
+  int i=value+1;
+  while (checkPrime(i)==false) {
+    i+=1;
   }
-  return result;
+   return i 
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   int sum = 0;
   for (int i=0; i < hbound; i++) {
-    if (i == 1 || i == 2 || i == 3 || i == 5 || i == 7) {
-      sum+=i;
-    } else if (i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0) {
+    if (checkPrime(i)){
       sum+=i;
     }
   }
